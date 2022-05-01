eabeb5e10ecf7fe20573eaf1b0654e18d3ce5f32
shankhs <shankholove@gmail.com>
2022-05-01 11:39:48 +0200

    Contest 785 Problem c

---
+++ codeforces/contest/785/c/c.cpp
@@ -0,0 +1,30 @@
+#ifdef ONPC
+#define _GLIBCXX_DEBUG
+#endif
+#include <bits/stdc++.h>
+#define sz(a) ((int)((a).size()))
+#define char unsigned char
+
+using namespace std;
+// mt19937 rnd(239);
+mt19937 rnd(chrono::steady_clock::now().time_since_epoch().count());
+
+typedef long long ll;
+typedef long double ld;
+
+int solve()
+{
+
+    return 0;
+}
+
+int32_t main()
+{
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    solve();
+#ifdef ONPC
+    cerr << endl
+         << "finished in " << clock() * 1.0 / CLOCKS_PER_SEC << " sec" << endl;
+#endif
+}
